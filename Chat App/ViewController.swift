7b1579a2c9edcbcc4bfc8a31ac89501318844912
hafied17 <hafiedmadrid6@gmail.com>
2022-02-12 06:26:26 +0700

    Initial Commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by hafied Khalifatul ash.shiddiqi on 12/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
