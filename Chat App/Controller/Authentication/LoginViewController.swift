d9e19c20ca28cdde7cb0c83f223b218a6e394c3d
dev vjb <iosdev@venturindo.com>
2022-04-04 13:51:38 +0700

    second commit

--- Chat App/Controller/Authentication/LoginViewController.swift
+++ Chat App/Controller/Authentication/LoginViewController.swift
@@ -18,6 +18,28 @@ class LoginViewController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let image = UIImage(named: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image!, textField: passwordTextField)
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        return tf
+    }()
+    
     // MARK: Lifecycle
 
     override func viewDidLoad() {
@@ -40,6 +62,12 @@ class LoginViewController: UIViewController {
         view.addSubview(logoImage)
         logoImage.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImage.setDimensions(width: 150, height: 150)
-
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 16, paddingRight: 16 )
     }
 }
